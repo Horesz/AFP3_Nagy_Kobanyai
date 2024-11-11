a7bde7ab12c1ac77a32f07ca3263b38bd816b2c3
SD4n1 <125566092+SD4n1@users.noreply.github.com>
2024-11-11 21:44:48 +0100

    DB_connection

---
+++ Project/KobanyaiPizzaLand/app/Http/Controllers/PizzaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Pizza;
+
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
+{
+    public function index()
+        {
+            $pizzas = Pizza::all();
+            return view('home', ['pizzas' => $pizzas]);
+        }
+}
