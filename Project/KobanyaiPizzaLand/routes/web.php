a3e79cff155991305e739c09b2dda0ebddbafbdd
Fuleee20 <bukovinszki.balint@gmail.com>
2024-11-12 20:01:30 +0100

    web.php módosítása

--- Project/KobanyaiPizzaLand/routes/web.php
+++ Project/KobanyaiPizzaLand/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReviewsController;
 
 Route::get('/', function () {
     return view('home');
@@ -15,3 +17,7 @@ Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
 
 Route::post('/cart/update/{id}', [CartController::class, 'updateQuantity'])->name('cart.update');
 Route::post('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/reviews', [ReviewsController::class, 'index']);
