2b11c210806681e2dabb653065f1b6b88e7ddf0a
Horesz <wc8j9d@std.uni-eszterhazy.hu>
2024-11-13 14:19:39 +0100

    Az egyetlen müködő váltazat ami mindent tartalmaz

--- Project/KobanyaiPizzaLand/resources/views/home.blade.php
+++ Project/KobanyaiPizzaLand/resources/views/home.blade.php
@@ -1,5 +1,19 @@
+<!-- resources/views/home.blade.php -->
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PizzaLand</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+
+    @include('header')
+    
+
     <section class="products">
-        <!-- Itt a main ágból a termékek listázása -->
         <div class="container">
             <h1>Pizzák</h1>
             <div class="product-list">
@@ -8,7 +22,7 @@
                         <h3>{{ $pizza->nev }}</h3>
                         <p>Ár: {{ $pizza->ar }} Ft</p>
                         <p class="product-description">Feltétek: {{ $pizza->feltet }}</p>
-                        <img src="{{ asset('images/' . strtolower(str_replace(' ', '_', $pizza->nev)) . '.jpg') }}" alt="{{ $pizza->nev }}">
+                        <img src="{{ asset('images/' . strtolower(str_replace(' ', '', $pizza->nev)) . '.jpg') }}" alt="{{ $pizza->nev }}">
                         <form action="{{ route('add.to.cart', $pizza->id) }}" method="POST">
                             @csrf
                             <button type="submit" class="btn product-button">Kosárhoz adás</button>
@@ -18,6 +32,8 @@
             </div>
         </div>
     </section>
+    
+
     <section class="reviews">
         <div class="container">
             <h2>Vásárlói Vélemények</h2>
--- Project/KobanyaiPizzaLand/routes/web.php
+++ Project/KobanyaiPizzaLand/routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PizzaController;
+
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
 
+// routes/web.php
 
+
+Route::get('/cart', [CartController::class, 'view'])->name('cart.view');
+Route::get('/', [PizzaController::class, 'index'])->name('pizza.index');
+
+Route::post('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
+
+Route::post('/cart/update/{id}', [CartController::class, 'updateQuantity'])->name('cart.update');
+Route::post('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
