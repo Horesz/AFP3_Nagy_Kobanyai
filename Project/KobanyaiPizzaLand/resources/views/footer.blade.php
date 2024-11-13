8c4b4798ab8c2aff92612dc93fc2352b0fac2dc6
gybence202 <gyorfibence1@gmail.com>
2024-11-13 13:01:12 +0100

    footer, header

---
+++ Project/KobanyaiPizzaLand/resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <p>Kapcsolat: +36 1 234 5678 | info@kobanyaipizza.hu</p>
+        <p>Kövess minket: Facebook | Instagram</p>
+        <p>&copy; 2024 Kőbányai Pizzéria. Minden jog fenntartva.</p>
+    </div>
+</footer>
