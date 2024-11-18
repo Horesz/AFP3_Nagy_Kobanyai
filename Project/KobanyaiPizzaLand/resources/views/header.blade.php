7571ecc15dd212de5f9c51002fde1cdeab432617
Fuleee20 <bukovinszki.balint@gmail.com>
2024-11-18 16:55:16 +0100

    header Login átírva és Register átírva

--- Project/KobanyaiPizzaLand/resources/views/header.blade.php
+++ Project/KobanyaiPizzaLand/resources/views/header.blade.php
@@ -6,7 +6,22 @@
             <a href="/">Menü</a>
             <a href="#">Akciók</a>
             <a href="#">Kapcsolat</a>
-            <a id="cart" href="{{ route('cart.view') }}"><i class="fas fa-shopping-bag"></i> Kosár {{ $cartTotal }} Ft</a>
+
+            @if (Route::has('login'))
+                @auth
+                    <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                    <li>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
+                @else
+                    <a class="log_reg" href="{{ route('login') }}">Belépés</a>
+                    <a class="log_reg" href="{{ route('register') }}">Regisztráció</a>
+                @endauth
+            @endif
+            <a id="cart" href="{{ route('cart.view') }}"><i class="fas fa-shopping-bag"></i> Kosár {{ $cartTotal ?? 0 }} Ft</a>
         </nav>
     </div>
 </header>
