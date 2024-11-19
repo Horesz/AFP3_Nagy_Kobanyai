949981d3317b46578d327436923c658b31814528
Fuleee20 <bukovinszki.balint@gmail.com>
2024-11-19 18:43:26 +0100

    hiba javítása

--- Project/KobanyaiPizzaLand/resources/views/auth/forgot-password.blade.php
+++ Project/KobanyaiPizzaLand/resources/views/auth/forgot-password.blade.php
@@ -14,14 +14,11 @@
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Elfelejtette a jelszavát? Semmi gond. Csak adja meg az email-címét, és elküldünk Önnek egy jelszó-visszaállítási linket, amely lehetővé teszi egy új jelszó beállítását.') }}
     </div>
-
-    <!-- Munkamenet állapota -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email cím -->
         <div>
             <x-input-label for="email" :value="__('Email cím')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
