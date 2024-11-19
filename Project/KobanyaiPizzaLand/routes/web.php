2e13a91bd748318c2e188050a92f32a3d98fc4fb
MG572 <martongergely2802@gmail.com>
2024-11-19 20:46:26 +0100

    Kapcsolat oldal
    Google map WIP

--- Project/KobanyaiPizzaLand/routes/web.php
+++ Project/KobanyaiPizzaLand/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
@@ -18,6 +19,8 @@ Route::get('/pizzas', [PizzaController::class, 'view'])->name('pizzas.view');
 
 Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->name('pizzas.show');
 
+
+
 Route::post('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
 Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
 
@@ -29,5 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.view');
 
 require __DIR__.'/auth.php';
