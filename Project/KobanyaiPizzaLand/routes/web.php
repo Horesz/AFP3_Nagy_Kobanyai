530022095a6d9143b15ad0f34a96c56c3cc5ce8e
SD4n1 <125566092+SD4n1@users.noreply.github.com>
2024-11-20 14:26:42 +0100

    dashboard

--- Project/KobanyaiPizzaLand/routes/web.php
+++ Project/KobanyaiPizzaLand/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
@@ -45,6 +46,7 @@ Route::get('/order/success', [OrderController::class, 'show'])->name('order.succ
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
