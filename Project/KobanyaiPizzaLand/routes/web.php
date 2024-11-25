71f385e55fc7c1a40ee7f1779f7c6f8611c8e2db
gybence202 <97533249+gybence202@users.noreply.github.com>
2024-11-25 20:41:17 +0100

    web.php

--- Project/KobanyaiPizzaLand/routes/web.php
+++ Project/KobanyaiPizzaLand/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\PizzaController;
@@ -7,7 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\NewsletterController;
+
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
