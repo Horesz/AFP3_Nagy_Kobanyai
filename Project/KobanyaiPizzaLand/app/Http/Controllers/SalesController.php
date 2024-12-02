032dd2bfcb9735da20fbeacc9c1a5f83500da44b
Fuleee20 <bukovinszki.balint@gmail.com>
2024-12-02 21:33:46 +0100

    statikus lista a sales.blade termékekhez

--- Project/KobanyaiPizzaLand/app/Http/Controllers/SalesController.php
+++ Project/KobanyaiPizzaLand/app/Http/Controllers/SalesController.php
@@ -8,18 +8,25 @@ use Illuminate\Support\Facades\File;
 
 class SalesController extends Controller
 {
-
-
-
-
-
     public function index()
     {
-        $images = File::files(public_path('images'));
-        $randomImages = collect($images)->random(20)->map(function ($file) {
-            return 'images/' . $file->getFilename();
-        });
+        $promotions = [
+            [
+                'id' => 1,
+                'name' => 'Pizza + Üdítő',
+                'price' => 3500,
+                'image' => 'pizzaüdítő.png',
+                'description' => 'Egy finom pizza és egy friss üdítő.',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Pizza+ Köbányai Sör',
+                'price' => 3800,
+                'image' => 'Margherita_kobanyai.png',
+                'description' => 'Extra kőbányai és a pizza tökéletes kombináció.',
+            ],
+        ];
 
-        return view('sales',['randomImages' => $randomImages]);
+        return view('sales', compact('promotions'));
     }
 }
